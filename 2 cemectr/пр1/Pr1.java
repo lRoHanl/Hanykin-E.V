d494ef68fb5e4cdfc12f190415267b01c2f33e0a
lRoHanl <113988432+lRoHanl@users.noreply.github.com>
2023-05-17 23:21:12 +0300

    Create Pr1.java

---
+++ 2 cemectr/пр1/Pr1.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Pr1 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int size1 = 2;
+        int size2 = 4;
+
+        int[][] matrix2 = new int[size1][size2];
+        
+        for (int i = 0; i < size2; i++) {
+            for (int j = 0; j < size1; j++) {
+                matrix2[j][i] = rnd.nextInt(10);
+                System.out.print(matrix2[j][i] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+
+}
